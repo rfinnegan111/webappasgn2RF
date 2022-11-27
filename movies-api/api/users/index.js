705061936c0d8161f883a45d9924dde796e1caab
Rebecca Finnegan <20093862@mail.wit.ie>
2022-11-27 21:20:46 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from './userModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -40,5 +41,48 @@ router.get('/', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
+
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
+    }
+});
+
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
+    }
+});
+
+
+  // register
+  router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {  //if action is 'register' then save to DB
+        await User(req.body).save();
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new user.',
+        });
+    }
+    else {  //Must be authenticating the!!! Query the DB and check if there's a match
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
+        }
+    }
+}));
 
 export default router;
--- movies-api/index.js
+++ movies-api/index.js
@@ -7,6 +7,15 @@ import usersRouter from './api/users';
 
 dotenv.config();
 
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
 const app = express();
 
 const port = process.env.PORT;
@@ -14,6 +23,7 @@ const port = process.env.PORT;
 app.use('/api/movies', moviesRouter);
 //Users router
 app.use('/api/users', usersRouter);
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
