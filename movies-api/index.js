0778e652421bc02d29429c323430a64954bc3f5b
Rebecca Finnegan <20093862@mail.wit.ie>
2022-11-27 21:13:24 +0000

    Mongo lab User Favourites

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import './db';
 import './seedData';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use('/api/movies', moviesRouter);
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
