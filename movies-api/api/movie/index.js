8fc9458c1b85143aa7de7f59ec0896904b375106
Rebecca Finnegan <20093862@mail.wit.ie>
2023-01-01 21:15:11 +0000

    Topic 11 - labs complete

--- movies-api/api/movie/index.js
+++ movies-api/api/movie/index.js
@@ -1,5 +1,8 @@
+import {
+    getUpcomingMovies
+  } from '../tmdb-api';
 import express from 'express';
-import { movies, movieReviews, movieDetails } from './moviesData';
+import { movies, movieReviews , movieDetails } from './moviesData';
 import uniqid from 'uniqid';
   import movieModel from './movieModel';
   import asyncHandler from 'express-async-handler';
@@ -66,5 +69,10 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 export default router;
